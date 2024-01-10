ad1037d25838935f27010706432482ce32f36a5a
JayParis <125691776+JayParis@users.noreply.github.com>
2024-01-10 15:36:43 +0000

    Update background-fragment.glsl

--- background-fragment.glsl
+++ background-fragment.glsl
@@ -1,1 +1,14 @@
-fragment
+precision highp float;
+
+uniform sampler2D uDiffuseMap;
+uniform float uTime;
+
+varying vec3 Normal;
+varying vec2 TexCoord;
+varying vec3 Position;
+
+void main()
+{
+    gl_FragColor =  vec4(TexCoord.x,TexCoord.y,0.0,1.0);
+}
+
